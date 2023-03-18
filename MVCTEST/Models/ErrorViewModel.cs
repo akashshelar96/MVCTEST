62d00c097f87b58b2e2a951e980b53bc9cfb8dfd
akashshelar96 <akashshelar1155@outlook.com>
2023-03-18 14:14:23 +0530

    Add project files.

---
+++ MVCTEST/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCTEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
